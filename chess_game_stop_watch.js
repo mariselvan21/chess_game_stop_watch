1c60583cc37476e3f5d091b7db9063d167f32889
mariselvan21 <mari20041999@gmail.com>
2022-08-05 17:15:35 +0530

    add files

---
+++ chess_game_stop_watch.js
@@ -0,0 +1,19 @@
+var a_minutes=document.getElementById("a_min");
+var a_seconds=document.getElementById("a_sec");
+var b_minutes=document.getElementById("b_min");
+var b_seconds=document.getElementById("b_sec");
+
+var select=document.getElementsByClassName("select");
+a_seconds.innerHTML="00";
+b_seconds.innerHTML="00";
+if(select.innnerHTML== "5")
+{
+    a_minutes.innerHTML="05";
+    b_seconds.innerHTML="05"
+}
+
+
+
+
+
+
